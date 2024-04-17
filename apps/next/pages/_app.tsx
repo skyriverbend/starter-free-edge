bcc2e0e3aaf8bced313bfb5d28ab6bcd4af1fc6c
Sky <1179717+skyriverbend@users.noreply.github.com>
2024-04-17 13:24:33 +1200

    Dynamically import polyfill

--- apps/next/pages/_app.tsx
+++ apps/next/pages/_app.tsx
@@ -1,17 +1,21 @@
 import '@tamagui/core/reset.css'
 import '@tamagui/font-inter/css/400.css'
 import '@tamagui/font-inter/css/700.css'
-import 'raf/polyfill'
 
 import { NextThemeProvider, useRootTheme } from '@tamagui/next-theme'
 import { Provider } from 'app/provider'
 import Head from 'next/head'
+import dynamic from 'next/dynamic'
 import React from 'react'
 import type { SolitoAppProps } from 'solito'
 
 if (process.env.NODE_ENV === 'production') {
   require('../public/tamagui.css')
 }
+
+dynamic(() => import('raf/polyfill'), {
+  ssr: false,
+})
 
 function MyApp({ Component, pageProps }: SolitoAppProps) {
   return (
